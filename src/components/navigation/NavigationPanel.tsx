9c42f6c25ffeac27c31657eb501cb3e830ac83a4
tehxueer29 <76723382+tehxueer29@users.noreply.github.com>
2024-08-17 23:01:45 +0800

    fix: update bg images

--- src/components/navigation/NavigationPanel.tsx
+++ src/components/navigation/NavigationPanel.tsx
@@ -1,11 +1,9 @@
 import StepNav from "../navigation/StepNav";
 
-const bg = `md:bg-[url('src/assets/images/bg-sidebar-desktop.svg')] bg-[url('src/assets/images/bg-sidebar-mobile.svg')]`;
-
 export default function NavigationPanel() {
   return (
     <div
-      className={`flex h-[172px] justify-center gap-x-4 gap-y-8 bg-cover bg-center px-8 pt-8 md:h-[568px] md:w-[274px] md:flex-col md:justify-normal md:pt-10 ${bg}`}
+      className={`bg-sidebar-mobile md:bg-sidebar-desktop flex h-[172px] justify-center gap-x-4 gap-y-8 bg-cover bg-center px-8 pt-8 md:h-[568px] md:w-[274px] md:flex-col md:justify-normal md:pt-10`}
     >
       <StepNav path="/step-1" title="Your Info" />
       <StepNav path="/step-2" title="Select Plan" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         primary: {
-          50: '#F8F9FE',
-          100: '#EEF5FF',
-          500: '#BDE1FF',
-          900: '#03295A'
+          50: "#F8F9FE",
+          100: "#EEF5FF",
+          500: "#BDE1FF",
+          900: "#03295A",
         },
         secondary: {
-          400: '#918DFC',
-          500: '#493CFF',
-          600: '#504C98'
+          400: "#918DFC",
+          500: "#493CFF",
+          600: "#504C98",
         },
         grey: {
-          500: '#DBDADF',
-          600: '#9D9DA5'
+          500: "#DBDADF",
+          600: "#9D9DA5",
         },
         red: {
-          500: '#B65161',
-          600: '#9E5B64'
-        }
-      }
-    }
+          500: "#B65161",
+          600: "#9E5B64",
+        },
+      },
+    },
+    backgroundImage: {
+      "sidebar-desktop": "url('./src/assets/images/bg-sidebar-desktop.svg')",
+      "sidebar-mobile": "url('./src/assets/images/bg-sidebar-mobile.svg')",
+    },
   },
-  plugins: []
+  plugins: [],
 };
